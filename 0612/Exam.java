c6ca71442807287a000ac5521887a4ea652e2b47
Edwin-J <gkdhaosl@naver.com>
2017-06-12 10:30:28 +0900

    0612_2

---
+++ 0612/Exam.java
@@ -0,0 +1,22 @@
+
+class Sunrin {
+	String s = "선린";
+	Sunrin() { s = s + "인터넷고"; }
+	String get() { return "멋진 선린인"; }
+}
+
+class Web extends Sunrin {
+	String s=  "웹운영과";
+	Web(){ s = super.s + s; };
+	String get() { return s; }
+}
+
+public class Exam {
+
+	public static void main(String[] args) {
+		Sunrin sun = new Web();
+		System.out.println(sun.s);
+		System.out.println(sun.get());
+	}
+
+}
---
+++ 0612/OverridingTest.java
@@ -0,0 +1,30 @@
+
+class Person {
+	private String phone;
+	
+	public void setPhone(String phone){
+		this.phone = phone;
+	}
+	
+	public String getPhone(){
+		return phone;
+	}
+}
+
+class Teacher extends Person {
+	
+	public String getPhone(){
+		return "선생님 전화번호 : " + super.getPhone();
+	}
+	
+}
+
+public class OverridingTest {
+
+	public static void main(String[] args) {
+		Person p = new Teacher();
+		p.setPhone("010-1234-5678");
+		System.out.println(p.getPhone());
+	}
+
+}
