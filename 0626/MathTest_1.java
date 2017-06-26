b135d6058757df574feb4b6e50b3818e4fa8e6c3
Edwin-J <gkdhaosl@naver.com>
2017-06-26 10:21:44 +0900

    0626_2

---
+++ 0626/MathTest_1.java
@@ -0,0 +1,12 @@
+
+public class MathTest_1 {
+
+	public static void main(String[] args) {
+		System.out.println("-2ÀÇ Àý´ñ°ª : " + Math.abs(-2));
+		System.out.println("2ÀÇ 3Á¦°ö : " + Math.pow(2, 3));
+		System.out.println("9ÀÇ Á¦°ö±Ù : " + Math.sqrt(9));
+		System.out.println("³­¼ö ¹ß»ý(0 ~ 9) : " + (int)Math.random() * 10);
+
+	}
+
+}
---
+++ 0626/MathTest_2.java
@@ -0,0 +1,10 @@
+
+public class MathTest_2 {
+
+	public static void main(String[] args) {
+		double dis = Math.sqrt(Math.pow(3, 2) + Math.pow(4, 2));
+		System.out.println("두 점 (0, 0)에서 (3,4)까지의 거리 : " + dis);
+
+	}
+
+}
